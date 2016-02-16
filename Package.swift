ef14c3fd212f92d21c991951313885d6fa4cc332
Paulo Faria <paulo.faria.rl@gmail.com>
2016-02-15 23:26:31 -0200

    it starts

---
+++ Package.swift
@@ -0,0 +1,19 @@
+import PackageDescription
+
+#if os(OSX)
+    let openSSLURL = "https://github.com/Zewo/COpenSSL-OSX.git"
+#else
+    let openSSLURL = "https://github.com/Zewo/COpenSSL.git"
+#endif
+
+let package = Package(
+    name: "HTTPSClient",
+    dependencies: [
+        .Package(url: "https://github.com/Zewo/TCPSSL.git", majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 2),
+        .Package(url: openSSLURL, majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/Zewo/CLibvenice.git", majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/Zewo/CURIParser.git", majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/Zewo/CHTTPParser.git", majorVersion: 0, minor: 2)
+    ]
+)
