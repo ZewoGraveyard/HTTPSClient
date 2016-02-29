d9b505b09a7e34647a032265bc595dfcbe4763a1
Paulo Faria <paulo.faria.rl@gmail.com>
2016-02-29 10:24:51 -0300

    bump to version 0.3

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     name: "HTTPSClient",
     dependencies: [
         .Package(url: "https://github.com/Zewo/TCPSSL.git", majorVersion: 0, minor: 2),
-        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 3),
         .Package(url: openSSLURL, majorVersion: 0, minor: 2),
         .Package(url: "https://github.com/Zewo/CLibvenice.git", majorVersion: 0, minor: 2),
         .Package(url: "https://github.com/Zewo/CURIParser.git", majorVersion: 0, minor: 2),
