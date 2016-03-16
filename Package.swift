dae0feeeb00ac3d16fa9fe906884b0a98d722b9f
antonmes <antonmes@users.noreply.github.com>
2016-03-16 08:19:58 +0300

    Swift 3 renaming

--- Package.swift
+++ Package.swift
@@ -1,19 +1,9 @@
 import PackageDescription
 
-#if os(OSX)
-    let openSSLURL = "https://github.com/Zewo/COpenSSL-OSX.git"
-#else
-    let openSSLURL = "https://github.com/Zewo/COpenSSL.git"
-#endif
-
 let package = Package(
     name: "HTTPSClient",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/TCPSSL.git", majorVersion: 0, minor: 2),
-        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 3),
-        .Package(url: openSSLURL, majorVersion: 0, minor: 2),
-        .Package(url: "https://github.com/Zewo/CLibvenice.git", majorVersion: 0, minor: 2),
-        .Package(url: "https://github.com/Zewo/CURIParser.git", majorVersion: 0, minor: 2),
-        .Package(url: "https://github.com/Zewo/CHTTPParser.git", majorVersion: 0, minor: 2)
+        .Package(url: "https://github.com/Zewo/TCPSSL.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 4),
     ]
 )
