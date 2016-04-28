d24081afa606ae849a9644e067bcf3b387c91168
Paulo Faria <paulo.faria.rl@gmail.com>
2016-04-28 15:58:46 -0300

    remote test, need to rethink

--- Tests/HTTPSClient/HTTPSClientTests.swift
+++ Tests/HTTPSClient/HTTPSClientTests.swift
@@ -2,13 +2,9 @@ import XCTest
 @testable import HTTPSClient
 
 class HTTPSClientTests: XCTestCase {
-    func testCocoapods() {
-        do {
-            let client = try Client(uri: "https://cocoapods.org:443")
-            let response = try client.get("/")
-            XCTAssertEqual(response.status, Status.ok)
-        } catch {
-            XCTFail("\(error)")
+    func testReality() {
+        func testReality() {
+            XCTAssert(2 + 2 == 4, "Something is severely wrong here.")
         }
     }
 }
@@ -16,7 +12,7 @@ class HTTPSClientTests: XCTestCase {
 extension HTTPSClientTests {
     static var allTests : [(String, HTTPSClientTests -> () throws -> Void)] {
         return [
-           ("testCocoapods", testCocoapods),
+           ("testReality", testReality),
         ]
     }
 }
